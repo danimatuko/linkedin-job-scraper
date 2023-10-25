1285163542ba2cbfed97ea2c4262584d17e21aba
Dani Matuko <danimatuko@outlook.com>
2023-10-25 19:58:16 +0300

    get full job details export that to a json file

--- scraper.js
+++ scraper.js
@@ -1,53 +1,67 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
 
 (async () => {
   // Launch the browser
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"],
+  });
 
   // Create a page
   const page = await browser.newPage();
-  const searchTerm = "Web devloper";
+  const searchTerm = "Web developer";
 
-  // Go to LinkedIn job search page
-  await page.goto(
-    `https://www.linkedin.com/jobs/search/?keywords=${searchTerm}`
-  );
-
-  // Wait for the job listings to load
-  await page.waitForSelector(".jobs-search__results-list li", {
-    visible: true,
-  });
-
-  // Extract job details
-  const jobList = await page.evaluate(() => {
-    const jobElements = document.querySelectorAll(
-      ".jobs-search__results-list li"
+  try {
+    // Go to LinkedIn job search page
+    await page.goto(
+      `https://www.linkedin.com/jobs/search/?keywords=${searchTerm}`
     );
 
-    const jobs = [];
+    // Wait for the job listings to load
+    await page.waitForSelector("a.base-card__full-link");
 
-    for (const jobElement of jobElements) {
-      const job = {};
-      job.title = jobElement
-        .querySelector(".base-search-card__title")
-        .textContent.trim();
+    // Extract job details
+    const jobList = await page.evaluate(async () => {
+      const jobElements = document.querySelectorAll("a.base-card__full-link");
+      const jobs = [];
 
-      job.company = jobElement
-        .querySelector(".base-search-card__subtitle")
-        .textContent.trim();
+      for (const jobElement of jobElements) {
+        jobElement.click();
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for details to load
 
-      job.location = jobElement
-        .querySelector(".job-search-card__location")
-        .textContent.trim();
+        const job = {};
+        job.title =
+          document
+            .querySelector("h2.top-card-layout__title")
+            ?.textContent.trim() || "N/A";
+        job.company =
+          document
+            .querySelector(".topcard__org-name-link")
+            ?.textContent.trim() || "N/A";
+        job.location =
+          document
+            .querySelector(".topcard__flavor topcard__flavor--bullet")
+            ?.textContent.trim() || "N/A";
+        job.description =
+          document
+            .querySelector(".show-more-less-html__markup")
+            ?.textContent.trim() || "N/A";
 
-      jobs.push(job);
-    }
+        jobs.push(job);
+        // window.history.back(); // Go back to the search results
+      }
 
-    return jobs;
-  });
+      return jobs;
+    });
 
-  console.log(jobList);
-
-  // Close the browser.
-  await browser.close();
+    // Write the job data to a JSON file
+    fs.writeFileSync("jobs.json", JSON.stringify(jobList, null, 2));
+    console.log("Job data saved to jobs.json");
+  } catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
+    await browser.close();
+  }
 })();
