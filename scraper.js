713074c17845d57a5a688014b7486257e00b82d0
Dani Matuko <danimatuko@outlook.com>
2023-10-28 14:26:34 +0300

    full code refactor & add loader while wating for data

--- scraper.js
+++ scraper.js
@@ -1,6 +1,26 @@
 import puppeteer from "puppeteer";
 import fs from "fs/promises";
 
+/**
+ * Display a loader in the console.
+ * @returns {NodeJS.Timeout} The interval used for the loader animation.
+ */
+const showLoader = () => {
+  const loader = ["|", "/", "-", "\\"];
+  let i = 0;
+
+  return setInterval(() => {
+    process.stdout.write(`\r${loader[i]} Scraping data, this might take a moment...`);
+    i = (i + 1) % loader.length;
+  }, 250);
+};
+
+/**
+ * Scrape job details from a job listing page.
+ * @param {puppeteer.Page} page - The Puppeteer page instance.
+ * @param {string} jobLink - The URL of the job listing page.
+ * @returns {Promise<object>} An object containing job details.
+ */
 const scrapeJobDetails = async (page, jobLink) => {
   const selectors = {
     title: "h1.topcard__title",
@@ -9,62 +29,54 @@ const scrapeJobDetails = async (page, jobLink) => {
     description: "section.show-more-less-html",
   };
 
-  // Navigate to the job's page
   await page.goto(jobLink, { waitUntil: "domcontentloaded" });
-
-  // Wait for a certain period to ensure details are loaded
   await page.waitForTimeout(5000);
 
-  // Extract job details
-  const title = await page.$eval(selectors.title, (el) => el.textContent.trim());
-  const company = await page.$eval(selectors.company, (el) => el.textContent.trim());
-  const location = await page.$eval(selectors.location, (el) => el.textContent.trim());
-  const description = await page.$eval(selectors.description, (el) =>
-    el.textContent
-      .replace(/\s+/g, " ")
-      .replace(/\b(Show more|Show less)\b/g, "")
-      .trim()
-  );
+  const job = {};
 
-  return {
-    title,
-    company,
-    location,
-    description,
-  };
+  for (const [key, selector] of Object.entries(selectors)) {
+    job[key] = await page.$eval(selector, (el) => el.textContent.trim());
+  }
+
+  job.description = job.description
+    .replace(/\s+/g, " ") // Remove extra whitespace
+    .replace(/\b(Show more|Show less)\b/g, "") // Remove "Show more" "Show less" from description
+    .trim();
+
+  return job;
 };
 
-(async () => {
-  // Launch a headless browser
+/**
+ * Main function to initiate the scraping process.
+ */
+const main = async () => {
+  const loaderInterval = showLoader();
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const searchTerm = "Web Developer";
 
-  // Navigate to the LinkedIn job search page with the specified search term
   await page.goto(`https://www.linkedin.com/jobs/search/?keywords=${searchTerm}`);
 
-  // Extract job links from the starting page
   const jobLinks = await page.$$eval("a.base-card__full-link", (links) => links.map((a) => a.href));
-
-  // Array to store job listing details
   const jobDetails = [];
 
-  // Iterate through job links and scrape details
   for (const jobLink of jobLinks) {
     const details = await scrapeJobDetails(page, jobLink);
     jobDetails.push(details);
   }
 
-  // Close the browser
   await browser.close();
 
-  // Save the scraped job data to a JSON file
+  clearInterval(loaderInterval);
+
   try {
-    // Convert jobDetails to JSON and write it to "jobData.json" asynchronously
     await fs.writeFile("jobData.json", JSON.stringify(jobDetails, null, 2, "utf-8"));
-    console.log("Data scraped and saved to jobData.json");
+    console.log("\n \x1b[32mData scraped and saved to jobData.json\x1b[0m");
   } catch (error) {
-    console.error(`Error writing to jobData.json: ${error}`);
+    console.error(`\n Error writing to jobData.json: ${error}`);
   }
-  console.log("Data scraped and saved to jobData.json");
-})();
+};
+
+// Call the main function to start the scraping process
+main();
